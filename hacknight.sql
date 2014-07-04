c5bbfb3c05b3d4c85428c5e1dae133c1011adf25
Reetika Roy <royritu21@yahoo.co.in>
2014-07-04 21:07:37 +0530

    Database Revised
    Revised Database Design

--- hacknight.sql
+++ hacknight.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 04, 2014 at 05:28 PM
+-- Generation Time: Jul 04, 2014 at 05:36 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `interests` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `interest` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -43,19 +44,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   `name` varchar(100) NOT NULL,
   `email` varchar(200) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_interest`
---
-
-CREATE TABLE IF NOT EXISTS `user_interest` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `intr_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
