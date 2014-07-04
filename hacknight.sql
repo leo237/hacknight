ef89730e07c2b40e15854d1e3d7f12557722164b
Reetika Roy <royritu21@yahoo.co.in>
2014-07-04 21:30:52 +0530

    Database 3.0
    Location added

--- hacknight.sql
+++ hacknight.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 04, 2014 at 05:36 PM
+-- Generation Time: Jul 04, 2014 at 06:00 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -43,6 +43,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email` varchar(200) NOT NULL,
+  `location` text NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
